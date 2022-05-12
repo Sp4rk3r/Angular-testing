a1ded0781935cc68d8135211b4769db74f890786
Sp4rk3r <koenrombout@hotmail.com>
2022-05-12 10:21:09 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-w8emvw
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-w8emvw)
---
+++ src/app/app.component.ts
@@ -0,0 +1,46 @@
+import { Component, VERSION } from '@angular/core';
+
+import { TodoService } from './todo.service';
+import { map, tap } from 'rxjs/operators';
+
+interface ToDo {
+  userId: number;
+  id: number;
+  title: string;
+  completed: boolean;
+}
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  name = 'Angular ' + VERSION.major;
+
+  data$ = this.todoService.todo$.pipe(
+    map((x: ToDo) => ({
+      ...x,
+      title: x.title + ' and more',
+    })),
+    tap((x) => {
+      console.log(x);
+    })
+  );
+
+  constructor(private todoService: TodoService) {}
+
+  // Result without ...x
+  // ===================
+  // title: "delectus aut autem and more"
+  //__proto__: Object
+  // only modify variable and array only exist with 'Title'
+  // Result with ...x
+  // ================
+  // completed: false
+  // id: 1
+  // title: "delectus aut autem and more"
+  // userId: 1
+  // __proto__: Object
+  // Copy the entire array and modify specific variable
+}
